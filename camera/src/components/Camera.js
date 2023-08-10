1ec8a90073c9706a8bb922f99ee2a03a70d8a700
Katerina Koukiou <kkoukiou@redhat.com>
2023-08-10 09:43:53 +0200

    Fix image saving

--- camera/src/components/Camera.js
+++ camera/src/components/Camera.js
@@ -2,8 +2,6 @@ import React, {useRef} from "react";
 import Webcam from "react-webcam";
 import axios from 'axios';
 
-
-
 const Camera = () => {
     console.log('Camera running');
     const webcamRef = React.useRef(null);
@@ -15,7 +13,7 @@ const Camera = () => {
       if (imageSrc.startsWith("data:image/jpeg;base64,")) {
         const imageBase64 = imageSrc.split(",")[1]; // convert the image to base64 (format for transmitting img in network)
         setImgSrc(imageSrc);
-        saveImage(imageBase64);
+        saveImage(imageSrc);
         console.log('worked');
       } else {
         console.log('not worked');
@@ -25,23 +23,16 @@ const Camera = () => {
 
       // post image to server
       const saveImage = async (imageBase64) => {
+        const image = imageBase64;
         try {
-          const response = await fetch("http://localhost:3020/api/upload", {
-            method: "POST",
-            // body: JSON.stringify({ image: imgSrc }),
-            headers: {
-              "Content-Type": "text/plain",
-              "Content-Length": imageBase64.length.toString(),
-            },
-            body: imageBase64
-        });
-        console.log('response', response);
+            const response = await axios.post("http://localhost:3020/api/upload", { image });
+            console.log('response', response);
 
-          if (response.ok) {
-            console.log("Image saved successfully");
-          } else {
-            console.error("Image could not be saved");
-          }
+            if (response.statusText === "OK") {
+                console.log(response.data.message);
+            } else {
+                console.error("Image could not be saved");
+            }
         } catch (error) {
           console.error("Error saving image:", error);
         }
--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
+const base64Img = require("base64-img");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3020;
 
-const allowedOrigin = "http://localhost:3001";
+const allowedOrigin = "http://localhost:3000";
 app.use(cors({
   origin: allowedOrigin
 }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 // Remove the express.json middleware
 
@@ -18,57 +21,16 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/upload", (req, res) => {
-  const chunks = [];
+    console.log('Image received:', req.body);
+    const { image } = req.body;
+    base64Img.img(image, './images', Date.now(), function(err, filepath) {
+        const pathArr = filepath.split('/');
+        const fileName = pathArr[pathArr.length - 1];
 
-  req.on("data", (chunk) => {
-    chunks.push(chunk);
-  });
-
-  req.on("end", () => {
-    const imageBuffer = Buffer.concat(chunks);
-    // Now you can work with the imageBuffer
-    console.log('Image received:', imageBuffer);
-
-    saveImageAsJpeg(imageBuffer);
-
-    res.status(200).json({ message: "Image received successfully" });
+        res.status(200).json({ message: "Image saved successfully: " + fileName });
   });
 });
-
-const saveImageAsJpeg = (imageBuffer) => {
-  const fileName = `received_image_${Date.now()}.jpg`; // Generate a unique filename
-  const filePath = path.join(__dirname, "images", fileName); // Path to the images folder
-
-  // Write the imageBuffer to the file
-  fs.writeFileSync(filePath, imageBuffer, "binary");
-  console.log(`Image saved as ${fileName}`);
-};
-
-
-// testing according to https://medium.com/@divinehycenth8/convert-a-base64-data-into-an-image-in-node-js-d82136576e35
-const base64 = fs.readFileSync("test.jpg", "base64");
-const buffer = Buffer.from(base64, "base64");
-fs.writeFileSync('new-path.jpg', buffer);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-
-// Set up Multer storage
-// const storage = multer.diskStorage({
-//     destination: path.join(__dirname, "uploads"),
-//     filename: function (req, file, cb) {
-//         cb(null, "captured-image.jpg");
-//     },
-// });
-
-// const upload = multer({ storage: storage });
-// console.log('testing  upload', upload);
-// app.post("/api/upload", upload.single("image"), (req, res) => {
-//     console.log('req', req);
-
-//     // res.json({ message: "Image saved successfully" });
-//     res.send("helle hat funktioniert")
-// });
